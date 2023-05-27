8852e22c1aab01df961606f78d21cbd6c79416d3
Vikram Bala <vikrambala2002@gmail.com>
2023-05-27 00:54:00 -0400

    added thread pool WIP code

--- include/thread_pool.hpp
+++ include/thread_pool.hpp
@@ -11,48 +11,54 @@
 
 #define MAX_THREADS 100
 
+
 template <typename T>
 class ThreadPool {
     public:
-        ThreadPool(uint16_t num_workers) {
-            if (pthread_mutexattr_init(&mutex_attributes) != 0) {
-                perror("pthread_mutex_attr_init() error");
-                exit(1);
-            } 
-            
-            if (pthread_mutexattr_settype(&mutex_attributes, PTHREAD_MUTEX_ERRORCHECK) != 0) {
-                perror("pthread_mutexattrr_settype() error");
-                exit(1);
-            }
-            
-            if (pthread_mutex_init(&lock, &mutex_attributes) != 0) {
-                perror("pthread_mutex_init() error");
-                exit(1);
-            }
+        /**
+         * @brief 
+         * 
+         */
+        typedef void (*workerFunction)(T);
+        
+        /**
+         * @brief Construct a new Thread Pool object
+         * 
+         * @param num_workers 
+         * @param wf 
+         */
+        ThreadPool(uint16_t num_workers, workerFunction wf);
 
-            if (pthread_cond_init(&cond_var, NULL) != 0) {
-                perror("pthread_cond_init() error");
-                exit(1);
-            }
+        /**
+         * @brief 
+         * 
+         * @param job 
+         */
+        void add_job(T job);
+        
+        /**
+         * @brief Destroy the Thread Pool object
+         * 
+         */
+        ~ThreadPool();
 
-            for (int i = 0; i < min(MAX_THREADS, num_workers); i++) {
-                pthread_t* thread_ID = (pthread_t*) malloc(sizeof(pthread_t);
-                if (thread == NULL) {
-                    perror("malloc error for pthread_t");
-                    exit(1);
-                }
-
-                pthread_create(&thread_ID, NULL, worker_function, NULL);
-            }
-        }
-    
 
     private:
-        std::queue<T> jobs;
-        std::vector<pthread_t> threads;
-        pthread_mutex_t lock;
-        pthread_mutexattr_t mutex_attributes;
-        pthread_cond_t cond_var;
+        std::queue<T> jobs; //Jobs to execute
+        std::vector<pthread_t*> threads; //All available threads in the pool
+
+        pthread_mutex_t lock; 
+        pthread_mutexattr_t mutex_attributes; // For job queue sync
+        pthread_cond_t cond_var; // For job queue sync
+        workerFunction wf; // The function that the user of this library wishes to execute in each Thread. 
+
+        /**
+         * @brief 
+         * 
+         * @param args 
+         * @return void* 
+         */
+        void* worker_wrapper(void* args);
 
 
 };
--- lib/thread_pool.cpp
+++ lib/thread_pool.cpp
@@ -6,3 +6,74 @@
 
 #include "../include/thread_pool.hpp"
 
+
+template <typename T>
+ThreadPool<T>::ThreadPool(uint16_t num_workers, workerFunction wf) {
+    if (wf == NULL) {
+        perror("workerFunction wf was null");
+        exit(1);
+    }
+    this->wf = wf;
+
+    if (pthread_mutexattr_init(&mutex_attributes) != 0) {
+        perror("pthread_mutex_attr_init() error");
+        exit(1);
+    } 
+    
+    if (pthread_mutexattr_settype(&mutex_attributes, PTHREAD_MUTEX_ERRORCHECK) != 0) {
+        perror("pthread_mutexattr_settype() error");
+        exit(1);
+    }
+    
+    if (pthread_mutex_init(&lock, &mutex_attributes) != 0) {
+        perror("pthread_mutex_init() error");
+        exit(1);
+    }
+
+    if (pthread_cond_init(&cond_var, NULL) != 0) {
+        perror("pthread_cond_init() error");
+        exit(1);
+    }
+
+    for (int i = 0; i < min(MAX_THREADS, num_workers); i++) {
+        pthread_t* thread_ID = (pthread_t*) malloc(sizeof(pthread_t));
+        if (thread == NULL) {
+            perror("malloc error for pthread_t");
+            exit(1);
+        }
+
+        pthread_create(&thread_ID, NULL, worker_wrapper, NULL);
+        threads.push_back(thread_ID);
+    }
+}
+
+template <typename T>
+void ThreadPool<T>::add_job(T job) {
+    pthread_mutex_lock(&lock);
+    jobs.push_back(job);
+    pthread_mutex_unlock(&lock);
+}
+
+template <typename T>
+ThreadPool<T>::~ThreadPool() {
+    for (pthread_t* t : threads) {
+        //free dynamically allocated stuff
+    }
+}
+
+
+template <typename T>
+void* ThreadPool<T>::worker_wrapper(void* args) {
+    for (;;) {
+        pthread_mutex_lock(&lock); //Prevent access to job queue
+        if (jobs.empty()) pthread_cond_wait(&cond_var, &lock);
+        
+
+
+    }
+    
+
+
+
+}
+
