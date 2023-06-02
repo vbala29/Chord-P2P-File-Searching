957bb637d8967c80dd13e65120f5d99c8a43a78e
Vikram Bala <vikrambala2002@gmail.com>
2023-06-01 22:38:15 -0400

    fixed ip recv issue

--- src/penn-chord.cpp
+++ src/penn-chord.cpp
@@ -80,6 +80,47 @@ void* CommandLineThread(void* args) {
   return NULL;
 }
 
+
+/** https://stackoverflow.com/questions/15653695/how-to-convert-ip-address-in-char-to-uint32-t-in-c
+ * Convert human readable IPv4 address to UINT32
+ * @param pDottedQuad   Input C string e.g. "192.168.0.1"
+ * @param pIpAddr       Output IP address as UINT32
+ * return 1 on success, else 0
+ */
+int ipStringToNumber (const char*       pDottedQuad,
+                              unsigned int *    pIpAddr)
+{
+   unsigned int            byte3;
+   unsigned int            byte2;
+   unsigned int            byte1;
+   unsigned int            byte0;
+   char              dummyString[2];
+
+   /* The dummy string with specifier %1s searches for a non-whitespace char
+    * after the last number. If it is found, the result of sscanf will be 5
+    * instead of 4, indicating an erroneous format of the ip-address.
+    */
+   if (sscanf (pDottedQuad, "%u.%u.%u.%u%1s",
+                  &byte3, &byte2, &byte1, &byte0, dummyString) == 4)
+   {
+      if (    (byte3 < 256)
+           && (byte2 < 256)
+           && (byte1 < 256)
+           && (byte0 < 256)
+         )
+      {
+         *pIpAddr  =   (byte3 << 24)
+                     + (byte2 << 16)
+                     + (byte1 << 8)
+                     +  byte0;
+
+         return 1;
+      }
+   }
+
+   return 0;
+}
+
 void* ReceiveThread(void* args) {
   int sockfd, newsockfd, portno;
   socklen_t clilen;
@@ -149,7 +190,9 @@ void* ReceiveThread(void* args) {
     pcm.Deserialize(b);
 
     //Add in thread pool once this actually works TODO
-    static_cast<PennChord*>(args)->RecvMessage(pcm, Ipv4Address(cli_addr.sin_addr.s_addr));
+    unsigned int* addrPtr = (unsigned int*) malloc(sizeof(unsigned int));
+    ipStringToNumber(inet_ntoa(cli_addr.sin_addr), addrPtr);
+    static_cast<PennChord*>(args)->RecvMessage(pcm, Ipv4Address(static_cast<uint32_t>(*addrPtr)));
   }
 
   return NULL;
